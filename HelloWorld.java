3dbfbe1bc800df4682ab1531645893bc3775a555
@Lahu_kumbhar <lahurajek@gmail.com>
2015-07-31 11:43:43 +0530

    Create HelloWorld.java

---
+++ HelloWorld.java
@@ -0,0 +1,9 @@
+public class HelloWorld
+{
+  public static void main(String[] args)
+  {
+    String name="Lahu kumbhar";
+    System.out.println("Welcome to java programming ...!"+name);
+  }
+  
+}
